d45874895638d13660f110b11b3ef1386689f561
Yusuf Aktan <yusufaktn@outlook.com>
2024-12-10 16:36:07 +0300

    spring init

---
+++ src/main/java/com/aktanyusuf/pagination_sorting/PaginationSortingApplication.java
@@ -0,0 +1,13 @@
+package com.aktanyusuf.pagination_sorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationSortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginationSortingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aktanyusuf/pagination_sorting/PaginationSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aktanyusuf.pagination_sorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationSortingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
